77482fb61b6447cf08fec8f9b995318044399567
oodrea <rjaemi@gmail.com>
2024-09-23 19:51:26 +0800

    implemented WK3 screen commands

--- csopesy-process-scheduler/main.cpp
+++ csopesy-process-scheduler/main.cpp
@@ -6,29 +6,36 @@
 void read_command(std::string input);
 
 void print_heading() {
-	std::cout << "  _____   _____   ____   _____   ______   _____ __     __\n";
-	std::cout << " / ____| / ____| / __ \\ |  __ \\ |  ____| / ____|\\ \\   / /\n";
-	std::cout << "| |     | (___  | |  | || |__) || |__   | (___   \\ \\_/ / \n";
-	std::cout << "| |      \\___ \\ | |  | ||  ___/ |  __|   \\___ \\   \\   /  \n";
-	std::cout << "| |____  ____) || |__| || |     | |____  ____) |   | |   \n";
-	std::cout << " \\_____||_____/  \\____/ |_|     |______||_____/    |_|   \n";
-	std::cout << "\033[32mHello, Welcome to CSOPESY commandline!\n"; // \033[32m = green color
-	std::cout << "\033[93mType \'exit\' to quit, \'clear\' to clear the screen\n";
-	std::cout << "\033[0m";
+    std::cout << "  _____   _____   ____   _____   ______   _____ __     __\n";
+    std::cout << " / ____| / ____| / __ \\ |  __ \\ |  ____| / ____|\\ \\   / /\n";
+    std::cout << "| |     | (___  | |  | || |__) || |__   | (___   \\ \\_/ / \n";
+    std::cout << "| |      \\___ \\ | |  | ||  ___/ |  __|   \\___ \\   \\   /  \n";
+    std::cout << "| |____  ____) || |__| || |     | |____  ____) |   | |   \n";
+    std::cout << " \\_____||_____/  \\____/ |_|     |______||_____/    |_|   \n";
+    std::cout << "\033[32mHello, Welcome to CSOPESY commandline!\n"; // \033[32m = green color
+    std::cout << "\033[93mType \'exit\' to quit, \'clear\' to clear the screen\n";
+    std::cout << "\033[0m";
 }
 
 int main() {
-	print_heading();
-	// Get User Input
-	std::string input;
-	while (true) {
-		std::cout << "Enter a command: ";
-		std::getline(std::cin, input);
-		if (input == "exit") {
-			break;
-		}
-		read_command(input); //located in shell.h
-	};
+    while (true) {
+        // Always print the header when returning to the main menu
+        print_heading();
 
-	return 0;
+        // Get user input
+        std::string input;
+        std::cout << "Enter a command: ";
+        std::getline(std::cin, input);
+
+        // Handle exit explicitly
+        if (input == "exit") {
+            std::cout << "Exiting the program...\n";
+            break;
+        }
+
+        // Pass the command to read_command function (shell.h)
+        read_command(input);
+    }
+
+    return 0;
 }
--- csopesy-process-scheduler/shell.h
+++ csopesy-process-scheduler/shell.h
@@ -1,43 +1,152 @@
 #pragma once
 
-// TODO: replace if-else 
-// https://chatgpt.com/share/fbf2eca5-47b1-42e7-b124-286805e30257
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <sstream>
+#include <chrono>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <Windows.h>
+#include <functional>
 
-//void initialize();
-//void screen(std::string arg1, std::string arg2);
-//void scheduler_test();
-//void scheduler_stop();
-//void report_util();
-//void clear();
+// Utility Functions
+namespace Utils {
 
-void print_heading();
+    // used for now
+    int generateRandomNumber(int lower, int upper) {
+        srand(time(0));
+        return rand() % (upper - lower + 1) + lower;
+    }
 
+    // get current timestamp as a string
+    std::string getCurrentTimestamp() {
+        auto now = std::chrono::system_clock::now();
+        auto now_c = std::chrono::system_clock::to_time_t(now);
+        std::tm local_tm;
+        localtime_s(&local_tm, &now_c);
+        std::ostringstream oss;
+        oss << std::put_time(&local_tm, "%m/%d/%Y, %I:%M:%S %p");
+        return oss.str();
+    }
+
+    // screen loop func
+    void runScreenLoop(std::function<void()> action) {
+        std::string command;
+        while (command != "exit") {
+            system("cls");
+            action();
+            std::cout << "\n  Type 'exit' to return back to main menu: ";
+            std::getline(std::cin, command);
+        }
+        system("cls");
+    }
+}
+
+// Process Class
+class Process {
+private:
+    std::string processName;
+    int currentLine;
+    int totalLines;
+    std::string creationTime;
+
+public:
+    // initializes process with a name and random instruction count
+    Process(std::string name) {
+        processName = name;
+        totalLines = Utils::generateRandomNumber(100, 200);
+        currentLine = totalLines - Utils::generateRandomNumber(20, 50);
+        creationTime = Utils::getCurrentTimestamp();
+    }
+
+    // display process information on screen
+    void drawConsole() {
+        auto showDetails = [&]() {
+            std::cout << "\n  --- SCREEN: " << processName << " ---\n";
+            std::cout << "  Process name: " << processName << "\n";
+            std::cout << "  Current Instruction: " << currentLine << '/' << totalLines << "\n";
+            std::cout << "  Created At: " << creationTime << "\n";
+            std::cout << "  ---------------------------\n";
+            };
+        Utils::runScreenLoop(showDetails);
+    }
+
+    const std::string& getName() const { return processName; }
+};
+
+// Process Management
+namespace ProcessManager {
+    std::vector<Process> processes;
+
+    // func to create new process and display its console
+    void createProcess(const std::string& name) {
+        Process newProcess(name);
+        processes.push_back(newProcess);
+        newProcess.drawConsole();
+    }
+
+    // find a process by name
+    Process* findProcess(const std::string& name) {
+        for (auto& process : processes) {
+            if (process.getName() == name) {
+                return &process;
+            }
+        }
+        return nullptr;
+    }
+
+    // redraw console for an existing process
+    void redrawProcess(const std::string& name) {
+        if (Process* process = findProcess(name)) {
+            process->drawConsole();
+        }
+        else {
+            auto showError = []() {
+                std::cout << "\n  Process not found! Cannot redraw\n";
+                };
+            Utils::runScreenLoop(showError);
+        }
+    }
+}
+
+// Command Handlers
 void read_command(std::string input) {
-	if (input == "initialize") {
-		std::cout << "initialize command recognized. Doing something\n";
-		//initialize();
-	}
-	else if (input.substr(0, 6) == "screen") {
-		std::cout << "screen command recognized. Doing something\n";
-		// TODO: split to chunks to process
-	}
-	else if (input == "scheduler-test") {
-		std::cout << "scheduler-test command recognized. Doing something\n";
-		//scheduler_test();
-	}
-	else if (input == "scheduler-stop") {
-		std::cout << "scheduler-stop command recognized. Doing something\n";
-		//scheduler_stop();
-	}
-	else if (input == "report-util") {
-		std::cout << "report-util command recognized. Doing something\n";
-		//report_util();
-	}
-	else if (input == "clear") {
-		system("cls");
-		print_heading();
-	}
-	else {
-		std::cout << "\'" << input << "\'" << " is not recognized as an internal or external command.\n";
-	}
+    if (input == "initialize") {
+        std::cout << "\n  initialize command recognized. Doing something...\n";
+        system("cls");
+    }
+    else if (input.substr(0, 9) == "screen -s") {
+        // create a new process screen
+        ProcessManager::createProcess(input.substr(10));
+    }
+    else if (input.substr(0, 9) == "screen -r") {
+        // redraw existing process screen
+        ProcessManager::redrawProcess(input.substr(10));
+    }
+    else if (input == "scheduler-test") {
+        std::cout << "\n  scheduler-test command recognized. Doing something...\n";
+        Sleep(2000);
+        system("cls");
+    }
+    else if (input == "scheduler-stop") {
+        std::cout << "\n  scheduler-stop command recognized. Doing something...\n";
+        Sleep(2000);
+        system("cls");
+    }
+    else if (input == "report-util") {
+        std::cout << "\n  report-util command recognized. Doing something...\n";
+        Sleep(2000);
+        system("cls");
+    }
+    else if (input == "clear") {
+        system("cls");
+    }
+    else {
+        std::cout << "\n  '" << input << "' is not recognized as an internal or external command. Clearing menu...\n";
+        Sleep(2000);
+        system("cls");
+    }
 }
